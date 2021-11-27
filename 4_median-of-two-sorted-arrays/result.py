136c775d5a610bd3030e98ddc49363e4c0a9040e
xiangcai <136688059@qq.com>
2021-11-27 13:34:01 +0800

    daily:20211127

---
+++ 4_median-of-two-sorted-arrays/result.py
@@ -0,0 +1,66 @@
+"""
+给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+
+算法的时间复杂度应该为 O(log (m+n)) 。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,3], nums2 = [2]
+输出：2.00000
+解释：合并数组 = [1,2,3] ，中位数 2
+示例 2：
+
+输入：nums1 = [1,2], nums2 = [3,4]
+输出：2.50000
+解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+示例 3：
+
+输入：nums1 = [0,0], nums2 = [0,0]
+输出：0.00000
+示例 4：
+
+输入：nums1 = [], nums2 = [1]
+输出：1.00000
+示例 5：
+
+输入：nums1 = [2], nums2 = []
+输出：2.00000
+ 
+
+提示：
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        set1 = list(nums1 + nums2)
+        set1.sort()
+        length = len(set1)
+        if length % 2 == 1:
+            return set1[length // 2]
+        else:
+            return (set1[length // 2 - 1] + set1[length // 2]) / 2
+
+
+if __name__ == "__main__":
+    nums1 = [1, 1]
+
+    nums2 = [1, 2]
+    s = Solution()
+    print(s.findMedianSortedArrays(nums1, nums2))
